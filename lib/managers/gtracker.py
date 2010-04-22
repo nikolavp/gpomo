bcc38eb11dea573f014d3c5b05d9a10c1975dd10
Eustaquio Rangel <taq@eustaquiorangel.com>
2010-04-22 17:31:25 -0300

    Complete task

--- lib/managers/gtracker.py
+++ lib/managers/gtracker.py
@@ -46,6 +46,7 @@ class Gtracker:
          id, proj, name, points, task_id, task_name = self.server.get_story(i)
          id          = str(int(task_id)*-1) if len(task_name)>0 else id
          task_desc   = (" - %s %s" % (_("Tarefa"),task_name)) if len(task_name)>0 else ""
+         print id, task_desc
          return [id,proj+": "+name+task_desc,None]
       except Exception as exc:
          print "Error returning task from Gtracker Dbus client: %s %s" % (exc,self.server)
@@ -53,14 +54,23 @@ class Gtracker:
          return None
 
    def start_task(self,id):
-      pass
+      if int(id)>=0:
+         print "start, changing a story: %s" % id
+      else:
+         print "a task (%s) cannot be started" % id
 
    def complete_task(self,id):
       try:
          if self.server==None:
             if not self.init_server():
                return -1
-         return self.server.complete_task(id,True)>0
+         if int(id)<0:
+            id = str(int(id)*-1)
+            print "completing a task: %s" % id
+            return self.server.complete_task(id,True)>0
+         else:
+            print "end, changing a story: %s" % id
+            return self.server.complete_story(id,True)>0
       except Exception as exc:
          print "Error completing task with id %s on Gtracker Dbus client: %s %s" % (id,exc,self.server)
          self.reset_server()
